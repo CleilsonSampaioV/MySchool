ee2011e61224e1445b28cbf788c803635ee8a55b
Cleilson Sampaio <jcleilsonsv@hotmail.com>
2021-01-27 22:13:40 -0300

    Initial commit

---
+++ src/MySchool.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MySchool.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MySchool.Infra.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MySchool.Infra.Data
+{
+    public class Class1
+    {
+    }
+}
